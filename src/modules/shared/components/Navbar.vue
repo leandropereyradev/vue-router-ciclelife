0f6789f73f50379a601f1e5a09e0eafffe901dba
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-08-16 20:53:12 +0200

    Params and props, HTTP requests and redirects, Custom RouterLinks

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
-    <router-link to="/">Pokemon List</router-link>
-    <router-link to="/id">Pokemon por ID</router-link>
-    <router-link to="/about">About</router-link>
+    <!-- <router-link :to="{ name: 'home' }">Pokemon List</router-link>
+    <router-link :to="{ name: 'pokemon-id', params: { id: 85 } }">Pokemon por ID</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link> -->
+
+    <CustomLinks v-for="link in links" :key="link.to" :link="link" />
   </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  data() {
+    return {
+      links: [
+        { to: '/home', name: 'Pokemons' },
+        { to: '/pokemon/50', name: 'Por ID' },
+        { to: '/abaut', name: 'About' },
+        { to: 'https://google.com', name: 'Google' },
+      ]
+    }
+  },
+  components: {
+    CustomLinks: defineAsyncComponent(() => import('./CustomLinks'))
+  }
+}
+</script>
+
 
 <style scoped>
 div {
--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
   {
     path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/home",
+    name: "home",
     component: () =>
       import(
         /* webpackChunkName:"ListPage" */ "@/modules/pokemon/pages/ListPage"
@@ -10,17 +15,24 @@ const routes = [
   },
   {
     path: "/about",
+    name: "about",
     component: () =>
       import(
         /* webpackChunkName:"AboutPage" */ "@/modules/pokemon/pages/AboutPage"
       ),
   },
   {
-    path: "/id",
+    path: "/pokemon/:id",
+    name: "pokemon-id",
     component: () =>
       import(
         /* webpackChunkName:"PokemonPage" */ "@/modules/pokemon/pages/PokemonPage"
       ),
+    props: (route) => {
+      const id = Number(route.params.id);
+
+      return isNaN(id) ? { id: 1 } : { id };
+    },
   },
   {
     path: "/:pathMatch(.*)*",
